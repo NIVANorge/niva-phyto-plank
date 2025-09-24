f652d3c0660a73189e04321c5624c4400c97d23b
Roar Brænden <roar.branden@niva.no>
2025-09-24 10:11:44 +0200

    Changes to Nivabase connection

--- R/nivaPhytoPlankton/R/open_plankton_connection.R
+++ R/nivaPhytoPlankton/R/open_plankton_connection.R
@@ -4,9 +4,8 @@
 #'
 #' @details This function will ask for username, password from the user.
 #' It will look for an installed driver using NIVA common install routines.
-#' It's also possible to set the driver path with environment variable ORACLE_DRIVER_PATH.
 #' It will connect to Nivadatabase production host, but this could be changed by setting
-#' environment variable ORACLE_DBQ.
+#' environment variable ORACLE_DNS.
 #' @export
 #'
 open_plankton_connection <- function() {
@@ -23,32 +22,15 @@ open_plankton_connection <- function() {
   user <- readline(prompt = "Enter Oracle username: ")
   password <- getPass::getPass("Enter Oracle password: ")
 
-  # Try to find a driver
-  drivers <- odbc::odbcListDrivers()
-  # Look for NIVA default driver name
-  driver_name <- "Oracle in OraClient19Home1"
-  if (!(driver_name %in% drivers$name)) {
-    driver_path <- Sys.getenv("ORACLE_DRIVER_PATH")
-    if (driver_path == "") {
-      # Try a default path for Jupyterhub usage
-      driver_path <- "/usr/lib/oracle/23/client64/lib/libsqora.so.23.1"
-    }
-    if (!file.exists(driver_path)) {
-      stop(paste("Oracle driver not found. Please set the environment variable ORACLE_DRIVER_PATH to the path of the driver."), call. = FALSE)
-    }
-    driver_name <- driver_path
-  }
-
-  # Get DBQ from environment variable or use default
-  dbq <- Sys.getenv("ORACLE_DBQ")
-  if (dbq == "") {
-    dbq <- "dbora-niva-prod01.niva.corp:1555/NIVABPRD"
+  # Get DNS from environment variable or use default
+  dns <- Sys.getenv("ORACLE_DNS")
+  if (dbs == "") {
+    dbs <- "NIVABASE"
   }
 
   # Create connection
   con <- DBI::dbConnect(odbc::odbc(),
-                        Driver = driver_name,
-                        DBQ = dbq,
+                        DNS = dns,
                         UID = user,
                         PWD = password,
                         encoding = "UTF-8")
